0d41d33e781d4e59bac0119fa34f1d1916a48607
JieniWu <jwu5@colgate.edu>
2025-11-17 16:57:17 -0500

    added analysis

---
+++ analyze_verify.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+df = pd.read_csv('data/predictions_verify.tsv', sep='\t')
+
+
+filtered_df = df[df['prob'] > 0.6]
+
+filtered_df = filtered_df[['textid', 'target', 'predicted', 'prob']]
+
+
+filtered_df.to_csv('filtered_predictions_verify.tsv', sep='\t', index=False)
+
+print("\nFiltered data saved to 'filtered_predictions_verify.tsv'")
+
+accuracy_data = []
+for name in ['Matthew', 'Luke', 'John', 'Paul', 'Peter','James', 'Jude']:
+    # Filter by target name
+    target_df = filtered_df[filtered_df['target'] == name]
+    
+    if len(target_df) > 0:
+        # Calculate percentage where target == predicted
+        correct = (target_df['target'] == target_df['predicted']).sum()
+        total = len(target_df)
+        percentage = (correct / total) * 100
+        
+        accuracy_data.append({
+            'target': name,
+            'percentage_correct': round(percentage, 2)
+        })
+    else:
+        accuracy_data.append({
+            'target': name,
+            'percentage_correct': 0.0
+        })
+
+accuracy_df = pd.DataFrame(accuracy_data)
+accuracy_df.to_csv('accuracy_by_target_verify.tsv', sep='\t', index=False)
+print("\nAccuracy analysis saved to 'accuracy_by_target.tsv'")
